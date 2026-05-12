297d659be52d76c53e827515302eadee0ba690f3
Manish Pandey <113515298+manishp9@users.noreply.github.com>
2026-05-12 17:49:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+dotenv.config();
+
 const connectDB = require("./config/db");
 connectDB();
-dotenv.config();
-const port = process.env.PORT;
 
 const router = require("./routes/routes");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("backend is working");
 });
 
-app.use(cors());
 app.use("/", router);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`backend is working on port ${port}`);
